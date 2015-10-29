028d6e018f556d1cc45e78fb57fb21bdd6e7ce16
Shawn Morrison <smorrison1986@gmail.com>
2015-10-28 21:00:36 -0500

    Creating TestVersionAssembly v1.0.0.0

---
+++ Ntegrity/NtegrityAssemblyDiff.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ntegrity.Models;
+
+namespace Ntegrity
+{
+    public class NtegrityAssemblyDiff
+    {
+        public List<ClassTypeData> AddedClasses = new List<ClassTypeData>();
+        public List<ClassTypeData> RemovedClasses = new List<ClassTypeData>();
+
+        public NtegrityAssemblyDiff(AssemblyInterfaceData oldAssembly, AssemblyInterfaceData newAssembly)
+        {
+            
+        }
+
+        private void GetAddedAndRemovedClasses(AssemblyInterfaceData oldAssembly, AssemblyInterfaceData newAssembly)
+        {
+            foreach (var oldClass in oldAssembly.Classes)
+            {
+                if (newAssembly.Classes.All(x => x.Name != oldClass.Name))
+                {
+                    RemovedClasses.Add(oldClass);
+                }
+            }
+            foreach (var newClass in newAssembly.Classes)
+            {
+                if (oldAssembly.Classes.All(x => x.Name != newClass.Name))
+                {
+                    AddedClasses.Add(newClass);
+                }
+            }
+        }
+    }
+}
